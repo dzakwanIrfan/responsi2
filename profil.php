3700a6a53810f82e246a09157d7da9b4d1245ec2
dzakwanIrfan <lexdani368@gamil.com>
2023-11-22 17:14:42 +0700

    menambahkan styling pada profil dan editprofil page

--- profil.php
+++ profil.php
@@ -9,19 +9,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="/css/profil.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Aleo&family=Habibi&family=Holtwood+One+SC&family=Inter&family=Outfit:wght@400;900&display=swap" rel="stylesheet">
 </head>
 <body>
-    <form action="profilEdit.php">
-        <img src="<?php if($_SESSION['gambar'] == ''){ echo "/css/img/sith.png"; }else{ echo "/css/img/$_SESSION[gambar]"; } ?>" alt="<?= $_SESSION['nama'] ?>" id="imagePreview">
-        <label for="nama">NAMA</label>
-            <input type="text" value="<?= $_SESSION['nama'] ?>" id="nama" readonly>
-        <label for="username">USERNAME</label>
-            <input type="text" value="<?= $_SESSION['username'] ?>" id="username" readonly>
-        <label for="bio">BIO</label>
-            <textarea  cols="30" rows="10" id="bio" readonly ><?= $_SESSION['bio'] ?></textarea>
-        <label for="password">PASSWORD</label>
-            <input type="password" value="<?= $_SESSION['password'] ?>" id="password" readonly>
-            <input type="submit" value="EDIT">
-    </form>
+<?php include('navbar.php'); ?>
+    <div class="profil-container">
+        <div class="banner"><img src="/css/img/profil-gmb.jpg" alt=""></div>
+        <div class="image-container">
+            <img src="<?php if($_SESSION['gambar'] == ''){ echo "/css/img/sith.png"; }else{ echo "/css/img/$_SESSION[gambar]"; } ?>" alt="" id="imagePreview">
+        </div>
+        <div class="profil-content">
+            <div class="profil">
+                <form action="profilEdit.php">
+                <div class="profil-group">
+                    <div class="profil-label"><label for="nama">NAMA</label></div>
+                    <div class="profil-content"><input type="text" value="<?= $_SESSION['nama'] ?>" id="nama" readonly></div>
+                </div>
+                <div class="profil-group">
+                    <div class="profil-label"><label for="username">USERNAME</label></div>
+                    <div class="profil-content"><input type="text" value="<?= $_SESSION['username'] ?>" id="username" readonly></div>
+                </div>
+                <div class="profil-group">
+                    <div class="profil-label"><label for="bio">BIO</label></div>
+                    <div class="profil-content"><textarea  cols="30" rows="10" id="bio" readonly ><?= $_SESSION['bio'] ?></textarea></div>
+                </div>
+                <div class="profil-group">
+                    <div class="profil-label"><label for="password">PASSWORD</label></div>
+                    <div class="profil-content"><input type="password" value="<?= $_SESSION['password'] ?>" id="password" readonly></div>
+                </div>
+                <div class="profil-group">
+                    <div class="profil-submit"><input type="submit" value="EDIT"></div>
+                </div>
+                </form>
+            </div>
+            <div class="img"><img src="/css/img/profil-finger.png" alt=""></div>
+        </div>
+    </div>
 </body>
 </html>
--- profilEdit.php
+++ profilEdit.php
@@ -36,7 +36,7 @@
             echo "<script>alert('Data berhasil diubah!');</script>";
             echo "<script>window.location.href = 'profil.php';</script>";
         }else{
-            echo "bodoh";
+            echo $query;
         }
 
     }
@@ -48,20 +48,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="/css/profil.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Aleo&display=swap" rel="stylesheet">
 </head>
 <body>
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
-        <input type="file" id="uploadInput" accept=".jpg, .png" name="gambar">
-        <img src="<?php if($_SESSION['gambar'] == ''){ echo "/css/img/sith.png"; }else{ echo "/css/img/$_SESSION[gambar]"; } ?>" alt="<?= $_SESSION['nama'] ?>" id="imagePreview">
-        <label for="nama">NAMA</label>
-            <input type="text" value="<?= $_SESSION['nama'] ?>" id="nama" name="nama">
-        <label for="username">USERNAME</label>
-            <input type="text" value="<?= $_SESSION['username'] ?>" id="username" name="username">
-        <label for="bio">BIO</label>
-            <textarea  cols="30" rows="10" id="bio" name="bio"><?= $_SESSION['bio'] ?></textarea>
-        <label for="password">PASSWORD</label>
-            <input type="password" value="<?= $_SESSION['password'] ?>" id="password" name="password">
-            <input type="submit" value="SIMPAN" name="ubah">
-    </form>
+<?php include('navbar.php'); ?>
+    <div class="profil-container">
+        <div class="banner"><img src="/css/img/profil-gmb.jpg" alt=""></div>
+        <div class="image-container">
+            <img src="<?php if($_SESSION['gambar'] == ''){ echo "/css/img/sith.png"; }else{ echo "/css/img/$_SESSION[gambar]"; } ?>" alt="" id="imagePreview">
+            <div class="overlay-text" id="imagePreview">Ubah Gambar</div>
+        </div>
+        <div class="profil-content">
+            <div class="profil">
+                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
+                    <input type="file" id="uploadInput" style="display: none;" accept=".jpg, .png" name="gambar">
+                <div class="profil-group">
+                    <div class="profil-label"><label for="nama">NAMA</label></div>
+                    <div class="profil-content"><input type="text" value="<?= $_SESSION['nama'] ?>" id="nama" name="nama"></div>
+                </div>
+                <div class="profil-group">
+                    <div class="profil-label"><label for="username">USERNAME</label></div>
+                    <div class="profil-content"><input type="text" value="<?= $_SESSION['username'] ?>" id="username" name="username"></div>
+                </div>
+                <div class="profil-group">
+                    <div class="profil-label"><label for="bio">BIO</label></div>
+                    <div class="profil-content"><textarea  cols="30" rows="10" id="bio" name="bio"><?= $_SESSION['bio'] ?></textarea></div>
+                </div>
+                <div class="profil-group">
+                    <div class="profil-label"><label for="password">PASSWORD</label></div>
+                    <div class="profil-content"><input type="password" value="<?= $_SESSION['password'] ?>" id="password" name="password"></div>
+                </div>
+                <div class="profil-group">
+                    <div class="profil-submit"><input type="submit" value="SIMPAN" name="ubah"></div>
+                </div>
+                </form>
+            </div>
+            <div class="img"><img src="css/img/profil-finger.png" alt=""></div>
+        </div>
+    </div>
 </body>
 </html>
