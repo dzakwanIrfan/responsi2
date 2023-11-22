61ec776f25021dc8efe3b00819e9447c87b8958e
dzakwanIrfan <lexdani368@gamil.com>
2023-11-22 16:12:54 +0700

    menambahkan styling pada register page

--- register.php
+++ register.php
@@ -29,17 +29,52 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/register.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lakki+Reddy&display=swap" rel="stylesheet">
 </head>
 <body>
-    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" id="register">
-        <input type="text" id="nama" name="nama" placeholder="Nama">
-        <input type="text" id="username" name="username" placeholder="Username">
-        <input type="password" id="password" name="password" placeholder="Password">
-        <input type="password" id="konfirmasi" name="konfirmasi" placeholder="Konfirmasi Password">
-        <input type="hidden" id="bio" name="bio" placeholder="bio" value="">
-        <button type="submit" name="submit">Register</button>
-    </form>
-        <small><p>Sudah memiliki akun? <a href="login.php">Login</a></p></small>
-
+    <div class="container">
+        <div class="glass">
+            <div class="container-foto">
+                <img src="css/img/reg-gmb.png" alt="yoda">
+            </div>
+            <div class="container-registrasi">
+                <h2>REGISTRASI</h2>
+                <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" id="register">
+                    <div class="input-group">
+                        <input type="text" id="nama" name="nama" placeholder="Nama" oninput="cekNama()">
+                    </div>
+                    <div class="input-group">
+                        <input type="text" id="username" name="username" placeholder="Username">
+                    </div>
+                    <div class="input-group">
+                        <input type="password" id="password" name="password" placeholder="Password">
+                    </div>
+                    <div class="input-group">
+                        <input type="password" id="konfirmasi" name="konfirmasi" placeholder="Konfirmasi Password">
+                    </div>
+                    <div class="input-group">
+                        <input type="hidden" id="bio" name="bio" placeholder="bio" value="">
+                    </div>
+                    <button type="submit" name="submit">Register</button>
+                </form>
+                <small><p>Sudah memiliki akun? <a href="login.php">Login</a></p></small>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
+
+<script>
+    // validasi form
+    const namaInput = document.getElementById('nama');
+    function cekNama() {
+        let regex = /^[A-Za-z ]+$/;
+        if (!regex.test(namaInput.value) && namaInput.value != "") {
+            alert("Nama tidak boleh memiliki angka atau simbol!");
+            namaInput.value = namaInput.value.replace(/[^A-Za-z ]/g, '');
+        }
+    }
+</script>
